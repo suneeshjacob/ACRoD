3e434196e60fb27c73cee03800153f1d21f3c8b8
suneeshjacob <suneeshjacob@gmail.com>
2023-05-07 14:57:36 +0530

    Update functions.py

--- new_version/functions.py
+++ new_version/functions.py
@@ -20,6 +20,15 @@ def all_joints_connected_to_the_link(M,linknumber):
     return kvtemp
 
 def all_links_connected_to_the_link(M,linknumber):
+    """
+    all_links_connected_to_the_link function gives a list of all the links connected to a given link for a given robot-topology matrix.
+
+    this function takes the robot-topology matrix and the link number as input arguments.
+
+    :param M: robot-topology matrix (of size nxn, to be given in numpy.matrix format).
+    :param linknumber: link number (it is to be indexed from 0, not from 1).
+    :return: a list of items specifying the indices of links with which the given link is connected, in ascending order of the link numbers.
+    """
     k = all_joints_connected_to_the_link(M,linknumber)
     k2 = k[:linknumber]+[9]+k[linknumber:]
     k3 = [i for i in range(len(k2)) if ((k2[i]!=0)&(k2[i]!=9))]
@@ -27,6 +36,14 @@ def all_links_connected_to_the_link(M,linknumber):
 
 # Function for getting all possible paths
 def all_paths(M):
+    """
+    all_paths function gives a list of all paths connecting the base link and the end-effector link for a given robot-topology matrix.
+
+    this function takes the robot-topology matrix as input argument.
+
+    :param M: robot-topology matrix (of size nxn, to be given in numpy.matrix format).
+    :return: a list of lists of items each corresponding to a path and specifying the sequence of links in that path that are connected from the base link to the end-effector link (the link numbers are indexed from 0, not from 1).
+    """
     M = numpy.array(M)
     P = []
     P.append([0]) # Base link number (link numbers starting from zero)
@@ -48,4 +65,52 @@ def all_paths(M):
     
     return P
 
+def get_all_combinations_of_two_links(M_size):
+    output_list = []
+    if M_size%2 == 0:
+        n = int(M_size/2)
+    else:
+        n = int((M_size-1)/2) + 1
+    
+    for i in range(1,n):
+        all_combinations_of_i = list(itertools.combinations(range(M_size),i))
+        for j in all_combinations_of_i:
+            other_set = set(range(M_size))-set(j)
+            output_list.append([set(j),other_set])
+    
+    if M_size%2 == 0:
+        list_of_all_combinations = list(itertools.combinations(range(M_size),n))
+        list_with_redundancy = []
+        for i in list_of_all_combinations:
+            other_set = set(range(M_size))-set(i)
+            list_with_redundancy.append([set(i),other_set])
+        confirmed_list = []
+        
+        for i in list_with_redundancy:
+            if i[0] not in confirmed_list and i[1] not in confirmed_list:
+                confirmed_list.append(i[0])
+                confirmed_list.append(i[1])
+                output_list.append(i)
+    return output_list
+
+def graph_adjacency_matrix_from_robot_topology_matrix(M):
+    A = M.copy()
+    B = numpy.matrix([[A[i,j] if j>i else 0 for j in range(A.shape[1])] for i in range(A.shape[0])])
+    C = B + B.T
+    return C
+
 # Function for getting information about superfluous DOF
+def superfluous(M):
+    S = []
+    if numpy.sum(M==4) > 2:
+        C = get_all_combinations_of_two_links(len(M)):
+        for c in C:
+            c1 = list(sorted(list(c[0])))
+            c2 = list(sorted(list(c[1])))
+            indices_list = c1 + c2
+            A = graph_adjacency_matrix_from_robot_topology_matrix(M)
+            D = (A[indices_list,:][:,indices_list])[:len(c1),len(c1):]
+
+
+
+    return S
