e1b1c099d38225a5b165fa184d1426a891efe1f4
suneeshjacob <suneeshjacob@gmail.com>
2023-05-10 12:34:14 +0530

    Update functions.py

--- new_version/functions.py
+++ new_version/functions.py
@@ -193,45 +193,48 @@ def from_P_to_P_tilde(P,M):
 
 
 def get_variables_list(M):
-    variables_list = []
+    variables_dict = {}
     for i in range(len(M)):
         for j in range(i+1,len(M)):
+            current_variables_list = []
             if M[i,j] == 0:
                 pass
             elif M[i,j] == 1:
-                variables_list.append(f'r_{i+1}_{j+1}_x')
-                variables_list.append(f'r_{i+1}_{j+1}_y')
-                variables_list.append(f'r_{i+1}_{j+1}_z')
-                variables_list.append(f'be_{i+1}_{j+1}')
-                variables_list.append(f'ph_{i+1}_{j+1}')
+                current_variables_list.append(f'r_{i+1}_{j+1}_x')
+                current_variables_list.append(f'r_{i+1}_{j+1}_y')
+                current_variables_list.append(f'r_{i+1}_{j+1}_z')
+                current_variables_list.append(f'be_{i+1}_{j+1}')
+                current_variables_list.append(f'ph_{i+1}_{j+1}')
             elif M[i,j] == 2:
-                variables_list.append(f'be_{i+1}_{j+1}')
-                variables_list.append(f'ph_{i+1}_{j+1}')
+                current_variables_list.append(f'be_{i+1}_{j+1}')
+                current_variables_list.append(f'ph_{i+1}_{j+1}')
             elif M[i,j] == 3:
-                variables_list.append(f'r_{i+1}_{j+1}_x')
-                variables_list.append(f'r_{i+1}_{j+1}_y')
-                variables_list.append(f'r_{i+1}_{j+1}_z')
-                variables_list.append(f'be_{i+1}_{j+1}')
-                variables_list.append(f'ph_{i+1}_{j+1}')
+                current_variables_list.append(f'r_{i+1}_{j+1}_x')
+                current_variables_list.append(f'r_{i+1}_{j+1}_y')
+                current_variables_list.append(f'r_{i+1}_{j+1}_z')
+                current_variables_list.append(f'be_{i+1}_{j+1}')
+                current_variables_list.append(f'ph_{i+1}_{j+1}')
             elif M[i,j] == 4:
-                variables_list.append(f'r_{i+1}_{j+1}_x')
-                variables_list.append(f'r_{i+1}_{j+1}_y')
-                variables_list.append(f'r_{i+1}_{j+1}_z')
+                current_variables_list.append(f'r_{i+1}_{j+1}_x')
+                current_variables_list.append(f'r_{i+1}_{j+1}_y')
+                current_variables_list.append(f'r_{i+1}_{j+1}_z')
             elif M[i,j] == 5:
-                variables_list.append(f'r_{i+1}_{j+1}_x')
-                variables_list.append(f'r_{i+1}_{j+1}_y')
-                variables_list.append(f'r_{i+1}_{j+1}_z')
-                variables_list.append(f'be_{i+1}_{j+1}')
-                variables_list.append(f'ph_{i+1}_{j+1}')
-                variables_list.append(f'de_{i+1}_{j+1}')
+                current_variables_list.append(f'r_{i+1}_{j+1}_x')
+                current_variables_list.append(f'r_{i+1}_{j+1}_y')
+                current_variables_list.append(f'r_{i+1}_{j+1}_z')
+                current_variables_list.append(f'be_{i+1}_{j+1}')
+                current_variables_list.append(f'ph_{i+1}_{j+1}')
+                current_variables_list.append(f'de_{i+1}_{j+1}')
             elif M[i,j] == 6:
-                variables_list.append(f'r_{i+1}_{j+1}_x')
-                variables_list.append(f'r_{i+1}_{j+1}_y')
-                variables_list.append(f'r_{i+1}_{j+1}_z')
-                variables_list.append(f'be_{i+1}_{j+1}')
-                variables_list.append(f'ph_{i+1}_{j+1}')
-                variables_list.append(f'p_{i+1}_{j+1}')
+                current_variables_list.append(f'r_{i+1}_{j+1}_x')
+                current_variables_list.append(f'r_{i+1}_{j+1}_y')
+                current_variables_list.append(f'r_{i+1}_{j+1}_z')
+                current_variables_list.append(f'be_{i+1}_{j+1}')
+                current_variables_list.append(f'ph_{i+1}_{j+1}')
+                current_variables_list.append(f'p_{i+1}_{j+1}')
             elif M[i,j] == 7:
-                variables_list.append(f'be_{i+1}_{j+1}')
-                variables_list.append(f'ph_{i+1}_{j+1}')
-    return variables_list
+                current_variables_list.append(f'be_{i+1}_{j+1}')
+                current_variables_list.append(f'ph_{i+1}_{j+1}')
+            if len(current_variables_list) > 0:
+                variables_dict[(i,j)] = current_variables_list
+    return variables_dict
