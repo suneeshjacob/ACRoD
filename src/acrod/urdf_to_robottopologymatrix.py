0101dfb7c69f9b6a7045045407acbc7bdd21e6e7
Suneesh Jacob <suneeshjacob@gmail.com>
2024-01-17 11:31:49 +0530

    Update urdf_to_robottopologymatrix.py

--- src/acrod/urdf_to_robottopologymatrix.py
+++ src/acrod/urdf_to_robottopologymatrix.py
@@ -13,7 +13,8 @@ def get_joints_from_urdf(root):
                     if child2.tag == 'child':
                         child_link = child2.attrib['link']
                 joints_info.append((child.attrib['name'], child.attrib['type'], parent_link, child_link,))
-    return numpy.unique(sorted(joints_info, key=lambda x:x[0]))
+    
+    return {i[0]:i[1:] for i in numpy.unique(sorted(joints_info, key=lambda x:x[0]))}
 
 def get_links_from_urdf(root):
     links_info = []
